27890645afd89478f1e2bd23d8ce240c52f3419e
JatCool <limpopo923@gmail.com>
2019-11-23 18:46:40 +0200

    Some updates 23-11-2019

--- app/src/main/java/com/example/jatcool/zno_on_math/activity/user/Registration.java
+++ app/src/main/java/com/example/jatcool/zno_on_math/activity/user/Registration.java
@@ -11,15 +11,16 @@ import android.widget.Toast;
 import com.example.jatcool.zno_on_math.connection.NetworService;
 import com.example.jatcool.zno_on_math.R;
 import com.example.jatcool.zno_on_math.entity.User;
+import com.example.jatcool.zno_on_math.util.Validation;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class Registration extends AppCompatActivity {
-EditText email,login,name,password,repassword;
-Button add_users;
-User user;
+    EditText email,login,name,password,repassword;
+    Button add_users;
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,29 +34,53 @@ User user;
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                user =  new User(name.getText().toString(),email.getText().toString(),password.getText().toString());
-                        NetworService.getInstance()
-                                .getJSONApi()
-                                .CreateUsers(user)
-                                .enqueue(new Callback<User>() {
-                                    @Override
-                                    public void onResponse(Call<User> call, Response<User> response) {
-                                         //user.setStatus(response.body().status);
-                                   //тут нужно будет добавить вывод сообщения о удачной регистрации и очищать поля
-                                        // желательно еще перекидывать на активити с авторизацией
-                                        finish();
-                                    }
+                        if(checkValdationData(name.getText().toString(),email.getText().toString(),password.getText().toString(),
+                                repassword.getText().toString())){
+                            user =  new User(name.getText().toString(),email.getText().toString(),password.getText().toString());
+                            NetworService.getInstance()
+                                    .getJSONApi()
+                                    .CreateUsers(user)
+                                    .enqueue(new Callback<User>() {
+                                        @Override
+                                        public void onResponse(Call<User> call, Response<User> response) {
+                                            //user.setStatus(response.body().status);
+                                            //тут нужно будет добавить вывод сообщения о удачной регистрации и очищать поля
+                                            // желательно еще перекидывать на активити с авторизацией
+                                            finish();
+                                        }
 
-                                    @Override
-                                    public void onFailure(Call<User> call, Throwable t) {
-                                        Toast.makeText(getApplicationContext(),"Ошибка!",Toast.LENGTH_LONG)
-                                                .show();
-                                    }
-                                });
+                                        @Override
+                                        public void onFailure(Call<User> call, Throwable t) {
+                                            Toast.makeText(getApplicationContext(),"Ошибка!",Toast.LENGTH_LONG)
+                                                    .show();
+                                        }
+                                    });
+                        }
                     }
                 }
         );
 
+
+    }
+    private boolean checkValdationData(String email,String name,String password,String rePassword){
+        boolean flag=true;
+        if(!Validation.isValidEmail(email)){
+            flag=false;
+            //show message incorrect email
+        }
+        if(!Validation.isValidName(name)){
+            flag=false;
+            //show message incorrect name
+        }
+        if(!Validation.isValidPasswor(password)){
+            flag=false;
+            //show message incorrect password
+        }
+        if(!Validation.isEqualsPassword(password,rePassword)){
+            flag=false;
+            //show message password dont equals
+        }
+        return flag;
     }
 
 
