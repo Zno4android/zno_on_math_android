74f1c4190937f2ba7828e647c666c5d2571b7e47
Bohdan <dreamteamdevopers@gmail.com>
2019-11-23 19:37:08 +0200

    bihdan 23-11-2019 registration validation v3

--- app/src/main/java/com/example/jatcool/zno_on_math/util/Validation.java
+++ app/src/main/java/com/example/jatcool/zno_on_math/util/Validation.java
@@ -1,14 +1,37 @@
 package com.example.jatcool.zno_on_math.util;
 
+import android.widget.Toast;
+
 import com.example.jatcool.zno_on_math.constants.RegexC;
+import com.example.jatcool.zno_on_math.connection.*;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class Validation {
+    private static boolean flagEmailExist=true;
     public static boolean isExistEmail(String str){
 
-        return false;
+        NetworService.getInstance()
+                .getJSONApi()
+                .isEmailExist(str)
+                .enqueue(new Callback<Boolean>() {
+                    @Override
+                    public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                        if(response.body()!=null) {
+                            flagEmailExist= response.body().booleanValue();
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<Boolean> call, Throwable t) {
+
+                    }
+                });
+        return flagEmailExist;
     }
     public static boolean isValidEmail(String str){
         Pattern pattern=Pattern.compile(RegexC.VALID_EMAIL);
