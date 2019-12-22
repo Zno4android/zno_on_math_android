69a197accb927fbb57cf6970606353e0defaef4a
JatCool <limpopo923@gmail.com>
2019-12-22 15:54:37 +0200

    Some updates 22-12-2019

---
+++ app/src/main/java/com/example/jatcool/zno_on_math/ui/theory/theory.java
@@ -0,0 +1,31 @@
+package com.example.jatcool.zno_on_math.ui.theory;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.jatcool.zno_on_math.R;
+
+
+public class theory extends Fragment {
+
+  TextView t;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_theory, container, false);
+        t = (TextView)view.findViewById(R.id.hi);
+        t.setText("Это фрагмент с теорией");
+        return view;
+    }
+
+
+}
